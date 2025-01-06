cc0fff840815db2319613818c621c7a550098c06
LimYuJi <znzn40452@gmail.com>
2025-01-06 11:22:02 +0900

    login, memberDAO

--- src/main/java/com/smhrd/Controller/LoginController.java
+++ src/main/java/com/smhrd/Controller/LoginController.java
@@ -1,19 +1,48 @@
 package com.smhrd.Controller;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
+import com.smhrd.Model.Member;
+import com.smhrd.Model.MemberDAO;
 
 @WebServlet("/LoginController")
 public class LoginController extends HttpServlet {
-	private static final long serialVersionUID = 1L;
+   private static final long serialVersionUID = 1L;
 
-	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-			
-	}
-
+   protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+   
+      // 1. 인코딩
+      request.setCharacterEncoding("UTF-8");
+      // 2. index.jsp에서 받아온 데이터를 꺼내오기
+      String id = request.getParameter("id");
+      String pw = request.getParameter("pw");
+      // 3. 데이터 처리하기(DB에 값을 보내서 원하는 조건 판단) Member DAO로 보내야한다.
+      MemberDAO dao = new MemberDAO();
+      // Member자료형의 vo라는 객체를 만든 이유 : id과 pw를 한번에 묶어서 전송하기 위함
+      Member vo = new Member();
+      vo.setId(id);
+      vo.setPw(pw);
+      
+      Member result = dao.login(vo);// DB에 접근하기 위해 만들어놓은 메소드를 사용하겠다.
+      // 4. 결과 출력하기 (result에 값이 있는지 없는지를 판별)
+      if (result != null) {
+         // result에 값이 있다면
+         // (1)세션 영역 활용!
+         HttpSession session = request.getSession();
+         session.setAttribute("info", result);
+         System.out.println("일치하는 값을 세션에 저장");
+      }
+      // result에 값이 없다면 => DB에 일치하는 로그인 정보가 없다. => index.jsp이동
+      // result에 값이 있다면 => session에 데이터를 저장한 후, index.jsp이동
+      response.sendRedirect("index.jsp");
+      
+   }
 }
--- src/main/java/com/smhrd/Model/MemberDAO.java
+++ src/main/java/com/smhrd/Model/MemberDAO.java
@@ -15,6 +15,24 @@ public class MemberDAO {
 	      int result = session.insert("join", vo);
 	      // 3. 세션 반납
 	      session.close();
-	      return result; // Servlet으로 DB의 결과값을 전달!
+	      return result; // Servlet으로 DB의 결과값을 전달   
 	   }
+	   
+	   
+	   
+	   
+	   
+	   
+	   
+	// 로그인을 위한 메소드
+	   public Member login(Member vo) {
+	      SqlSession session = factory.openSession(true);
+	      // sql문장을 실행
+	      // 회원이 입력한 id와 pw가 DB에 있는 데이터 중에서 일치하는 것이 있는지 확인!->select
+	      // session.selectOne("login", "memberMapper.xml과 연결고리 = id값", 같이 보내는 데이터);
+	      Member result = session.selectOne("login", vo);
+	      session.close();
+	      return result;
+	   }
+	   
 }
