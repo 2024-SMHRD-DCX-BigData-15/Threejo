c573a0a71848acbb24f75fea8cdbf84adaa26004
LimYuJi <znzn40452@gmail.com>
2025-01-09 11:51:13 +0900

    BoardDelete commit

--- src/main/java/com/smhrd/Controller/BoardDelete.java
+++ src/main/java/com/smhrd/Controller/BoardDelete.java
@@ -7,13 +7,23 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.smhrd.Model.BoardDAO;
+
 @WebServlet("/BoardDelete")
 public class BoardDelete extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-	
+		  response.setCharacterEncoding("UTF-8");
+	      int idx = Integer.parseInt(request.getParameter("idx"));
+	      
+	      BoardDAO dao = new BoardDAO();
+	      int result = dao.delete(idx);
+	      
+	      if(result>0) {
+	         response.sendRedirect("boardlist.jsp");
+	      } else {
+	         response.sendRedirect("boardcontent?"+idx);
+	    }	   
 	}
-
 }
