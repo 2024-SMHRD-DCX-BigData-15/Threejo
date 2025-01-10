1f14ddfacba20bcbe79d1f988ede963331596fce
LimYuJi <znzn40452@gmail.com>
2025-01-10 09:11:26 +0900

    commit01

--- src/main/java/com/smhrd/Controller/CheckIdController.java
+++ src/main/java/com/smhrd/Controller/CheckIdController.java
@@ -16,14 +16,12 @@ public class CheckIdController extends HttpServlet {
 
 	protected void service(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		System.out.println("실행됨");
 		// 중복된 주소를 판별하기 위한 기능
 		// 0. 한글 인코딩
 		request.setCharacterEncoding("UTF-8");
 		// 1. 주소 중복 체크
 		String user_id = request.getParameter("user_id");
 		MemberDAO dao = new MemberDAO();
-		System.out.println("실행됨");
 		
 		// MemberDao에서 checkAddr 메소드 생성 -> mapper까지 연결
 		// sql문 : login과 비슷한 맥락으로 유추! (집 주소가 DB에 동일한 값이 있는지)
