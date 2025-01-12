0c2abaccbdc4008828b7fc47859433c255ba5da9
BaeChangHwan <bksm12@naver.com>
2025-01-12 14:00:24 +0900

    ProposalWriteController

--- src/main/java/com/smhrd/Controller/ProposalWriteController.java
+++ src/main/java/com/smhrd/Controller/ProposalWriteController.java
@@ -10,14 +10,14 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class ProposalController
  */
-@WebServlet("/ProposalController")
-public class ProposalController extends HttpServlet {
+@WebServlet("/ProposalWriteController")
+public class ProposalWriteController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public ProposalController() {
+    public ProposalWriteController() {
         super();
         // TODO Auto-generated constructor stub
     }
